6e701e029ec8bef53e410e6618eb62da6c462bc8
IndieFox98 <ttashima7@gmail.com>
2019-02-26 20:50:32 -0800

    Project 3 Deliverable 2 with Strip Tags

--- inc/create/content.inc.php
+++ inc/create/content.inc.php
@@ -15,59 +15,68 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     if (empty($_POST['first'])) {
         array_push($error_bucket,"<p>A first name is required.</p>");
     } else {
-        # Old way
+        # Older way
         #$first = $_POST['first'];
+        # Old way
+        # $first = $db->real_escape_string($_POST['first']);
         # New way
-        $first = $db->real_escape_string($_POST['first']);
+        $first = $db->real_escape_string(strip_tags($_POST['first']));
     }
     if (empty($_POST['last'])) {
         array_push($error_bucket,"<p>A last name is required.</p>");
     } else {
         #$last = $_POST['last'];
-        $last = $db->real_escape_string($_POST['last']);
+        #$last = $db->real_escape_string($_POST['last']);
+        $last = $db->real_escape_string(strip_tags($_POST['last']));
     }
     if (empty($_POST['id'])) {
         array_push($error_bucket,"<p>A student ID is required.</p>");
     } else {
         #$id = $_POST['id'];
-        $id = $db->real_escape_string($_POST['id']);
+        #$id = $db->real_escape_string($_POST['id']);
+        $id = $db->real_escape_string(strip_tags($_POST['id']));
     }
     if (empty($_POST['email'])) {
         array_push($error_bucket,"<p>An email address is required.</p>");
     } else {
         #$email = $_POST['email'];
-        $email = $db->real_escape_string($_POST['email']);
+        #$email = $db->real_escape_string($_POST['email']);
+        $email = $db->real_escape_string(strip_tags($_POST['email']));
     }
     if (empty($_POST['phone'])) {
         array_push($error_bucket,"<p>A phone number is required.</p>");
     } else {
         #$phone = $_POST['phone'];
-        $phone = $db->real_escape_string($_POST['phone']);
+        #$phone = $db->real_escape_string($_POST['phone']);
+        $phone = $db->real_escape_string(strip_tags($_POST['phone']));
     }
     if (empty($_POST['gpa']) && $_POST['gpa'] != "0") {  # work around interpreting 0 as empty
         array_push($error_bucket,"<p>A GPA is required.</p>");
     } else {
         #$gpa = $_POST['gpa'];
-        $gpa = $db->real_escape_string($_POST['gpa']);
+        #$gpa = $db->real_escape_string($_POST['gpa']);
+        $gpa = $db->real_escape_string(strip_tags($_POST['gpa']));
     }
     if (!isset($_POST['aid']) || empty($_POST['aid']) && $_POST['aid'] != "0") {  # work around interpreting 0 as empty
         array_push($error_bucket,"<p>Please answer the Financial Aid section.</p>");
     } else {
         #$aid = $_POST['aid'];
-        $aid = $db->real_escape_string($_POST['aid']);
+        #$aid = $db->real_escape_string($_POST['aid']);
+        $aid = $db->real_escape_string(strip_tags($_POST['aid']));
     }
     if (empty($_POST['program'])) {
         array_push($error_bucket,"<p>Please select your degree program.</p>");
     } else {
         #$program = $_POST['program'];
-        $program = $db->real_escape_string($_POST['program']);
+        #$program = $db->real_escape_string($_POST['program']);
+        $program = $db->real_escape_string(strip_tags($_POST['program']));
     }
 
     // If we have no errors than we can try and insert the data
     if (count($error_bucket) == 0) {
         // Time for some SQL
         $sql = "INSERT INTO $db_table (first_name,last_name,student_id,email,phone,gpa,financial_aid,degree_program) ";
-        $sql .= "VALUES ('$first','$last',$id,'$email','$phone','$gpa','$aid','$program')";
+        $sql .= "VALUES ('$first','$last',$id,'$email','$phone',$gpa,$aid,'$program')";
 
         // comment in for debug of SQL
         // echo $sql;
